a81ac9ed303f31fae6ac4a61511a6540d693e5ef
Nicolas Garay Grau <nicolasgaray.2295@gmail.com>
2024-07-29 11:17:54 -0300

    Subiendo proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
 
   return (
-<<<<<<< HEAD
       <BrowserRouter>
           <PropiedadesProvider>
             <MensajeProvider>
@@ -48,35 +47,6 @@ function App() {
           </PropiedadesProvider>
         <SpeedInsights />
       </BrowserRouter>
-=======
-    <BrowserRouter>
-      <PropiedadesProvider>
-        <MensajeProvider>
-          <AgenteProvider>
-            <BlogProvider>
-              <Routes>
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<Inicio/>}/>
-                  <Route path="login" element={<Login />}/>
-                  <Route path="catalogo" element={<Catalogo />}/>
-                  <Route path="contacto" element={<Contacto />}/>
-                  <Route path="nosotros" element={<Nosotros />}/>
-                  <Route path="blog" element={<Blog />}/>
-                  <Route path="publicacion/:id" element={<VerPublicacion />}/>
-                  <Route path="propiedad/:id" element={<VerPropiedad />}/>
-                  <Route path="confirmar/:token" element={<ConfirmarCuenta />}/>
-                </Route>
-                <Route path="/admin" element={<RutaAdmin />}>
-                  <Route index element={<Admin />}/>      
-                </Route>
-              </Routes>
-            </BlogProvider>
-          </AgenteProvider>
-        </MensajeProvider>
-      </PropiedadesProvider>
-      <SpeedInsights />
-    </BrowserRouter>
->>>>>>> ace0c34 (Arreglando detalles)
   )
 }
 
